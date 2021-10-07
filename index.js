2c00bbb597e13f14b7dfc893524aa254282a6e3f
Juan Carlo Magat <magatjuancarlo@gmail.com>
2021-10-07 12:33:17 -0700

    Add GraphQL and implement getPosts in GraphQL

--- index.js
+++ index.js
@@ -3,9 +3,54 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv/config");
 const app = express();
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList,
+} = require("graphql");
+const { graphqlHTTP } = require("express-graphql");
+
+const Post = require("./models/Post");
 
 app.use(express.json());
 app.use(cors());
+
+const PostType = new GraphQLObjectType({
+  name: "Post",
+  fields: () => ({
+    _id: { type: GraphQLString },
+    title: { type: GraphQLString },
+    message: { type: GraphQLString },
+    postedOn: { type: GraphQLString },
+    postedBy: { type: GraphQLString },
+    likes: { type: GraphQLInt },
+  }),
+});
+
+const RootQueryType = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: () => ({
+    posts: {
+      type: new GraphQLList(PostType),
+      resolve: async () => {
+        const posts = await Post.find();
+        return posts;
+      },
+    },
+  }),
+});
+
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+});
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
+});
+
+app.use("/graphql", graphqlHTTP({ schema: schema, graphiql: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
