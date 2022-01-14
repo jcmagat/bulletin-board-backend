b858700f2f6e2badc849beb75e512e29cc4ad51b
Juan Carlo Magat <magatjuancarlo@gmail.com>
2022-01-14 10:34:08 -0800

    Add error handling to community resolvers

--- src/resolvers/community.js
+++ src/resolvers/community.js
@@ -1,118 +1,145 @@
 const pool = require("../db");
+const { ApolloError, AuthenticationError } = require("apollo-server-express");
 
 /* ========== Query Resolvers ========== */
 
 exports.getAllCommunities = async (parent, args) => {
-  const query = await pool.query(
-    `SELECT community_id, name, title, description, created_at 
-    FROM communities`
-  );
+  try {
+    const query = await pool.query(
+      `SELECT community_id, name, title, description, created_at 
+      FROM communities`
+    );
 
-  const communities = query.rows;
+    const communities = query.rows;
 
-  return communities;
+    return communities;
+  } catch (error) {
+    throw new ApolloError("Internal server error");
+  }
 };
 
 exports.getCommunity = async (parent, args) => {
-  const name = args.name;
+  try {
+    const name = args.name;
 
-  const query = await pool.query(
-    `SELECT community_id, name, title, description, created_at 
+    const query = await pool.query(
+      `SELECT community_id, name, title, description, created_at 
     FROM communities 
     WHERE name = ($1)`,
-    [name]
-  );
+      [name]
+    );
 
-  const community = query.rows[0];
+    const community = query.rows[0];
 
-  return community;
+    return community;
+  } catch (error) {
+    throw new ApolloError("Internal server error");
+  }
 };
 
 // Child resolver for Community to get community's posts
 exports.getCommunityPosts = async (parent, args) => {
-  const community_id = parent.community_id;
+  try {
+    const community_id = parent.community_id;
 
-  const query = await pool.query(
-    `SELECT post_id, title, description, posts.user_id, username, 
+    const query = await pool.query(
+      `SELECT post_id, title, description, posts.user_id, username, 
       community_id, age(now(), posts.created_at) 
     FROM posts 
       INNER JOIN users 
       ON (posts.user_id = users.user_id)
     WHERE community_id = ($1)`,
-    [community_id]
-  );
+      [community_id]
+    );
 
-  const posts = query.rows;
+    const posts = query.rows;
 
-  return posts;
+    return posts;
+  } catch (error) {
+    throw new ApolloError("Internal server error");
+  }
 };
 
 // Child resolver for Community to get community's members
 exports.getCommunityMembers = async (parent, args) => {
-  const community_id = parent.community_id;
+  try {
+    const community_id = parent.community_id;
 
-  const query = await pool.query(
-    `SELECT user_id, username, created_at 
+    const query = await pool.query(
+      `SELECT user_id, username, created_at 
     FROM users 
     WHERE user_id IN (
       SELECT user_id 
       FROM members 
       WHERE community_id = ($1)
     )`,
-    [community_id]
-  );
+      [community_id]
+    );
 
-  const members = query.rows;
+    const members = query.rows;
 
-  return members;
+    return members;
+  } catch (error) {
+    throw new ApolloError("Internal server error");
+  }
 };
 
 /* ========== Mutation Resolvers ========== */
 
 exports.join = async (parent, args, { req, res }) => {
   if (!req.isAuth) {
-    throw new Error("Not authenticated");
+    throw new AuthenticationError("Not authenticated");
   }
 
-  const community_id = args.community_id;
-  const user_id = req.user.user_id;
+  try {
+    const community_id = args.community_id;
+    const user_id = req.user.user_id;
 
-  const query = await pool.query(
-    `WITH x AS (
+    const query = await pool.query(
+      `WITH x AS (
       INSERT INTO members (community_id, user_id) 
-      VALUES ($1, $2)
+      VALUES ($1, $2) 
+      ON CONFLICT ON CONSTRAINT members_pkey 
+      DO NOTHING
     )
     SELECT community_id, name, title, description, created_at 
     FROM communities 
     WHERE community_id = ($1)`,
-    [community_id, user_id]
-  );
+      [community_id, user_id]
+    );
 
-  const community = query.rows[0];
+    const community = query.rows[0];
 
-  return community;
+    return community;
+  } catch (error) {
+    throw new ApolloError("Internal server error");
+  }
 };
 
 exports.leave = async (parent, args, { req, res }) => {
   if (!req.isAuth) {
-    throw new Error("Not authenticated");
+    throw new AuthenticationError("Not authenticated");
   }
 
-  const community_id = args.community_id;
-  const user_id = req.user.user_id;
+  try {
+    const community_id = args.community_id;
+    const user_id = req.user.user_id;
 
-  const query = await pool.query(
-    `WITH x AS (
+    const query = await pool.query(
+      `WITH x AS (
       DELETE FROM members 
       WHERE community_id = ($1) AND user_id = ($2)
     )
     SELECT community_id, name, title, description, created_at 
     FROM communities 
     WHERE community_id = ($1)`,
-    [community_id, user_id]
-  );
+      [community_id, user_id]
+    );
 
-  const community = query.rows[0];
+    const community = query.rows[0];
 
-  return community;
+    return community;
+  } catch (error) {
+    throw new ApolloError("Internal server error");
+  }
 };
