5324bb7a7ab3cbc98dae7a863475835d032cd0e7
Juan Carlo Magat <magatjuancarlo@gmail.com>
2022-02-26 10:51:14 -0800

    Check if user is not deleted on authentication

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
+const pool = require("../database");
 
-exports.authenticateToken = (req, res, next) => {
+exports.authenticateToken = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
   if (!authHeader) {
     req.isAuth = false;
@@ -13,8 +14,19 @@ exports.authenticateToken = (req, res, next) => {
     return next();
   }
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
-    if (err) {
+  try {
+    const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user_id = payload.user_id;
+    const query = await pool.query(
+      `SELECT user_id 
+      FROM users 
+      WHERE user_id = ($1)`,
+      [user_id]
+    );
+
+    const user = query.rows[0];
+    if (!user) {
       req.isAuth = false;
       return next();
     }
@@ -22,5 +34,8 @@ exports.authenticateToken = (req, res, next) => {
     req.isAuth = true;
     req.user = payload;
     next();
-  });
+  } catch (error) {
+    req.isAuth = false;
+    return next();
+  }
 };
