8b61345a2bd727356b120507f162d8f6df68c420
Juan Carlo Magat <magatjuancarlo@gmail.com>
2022-03-10 09:31:27 -0800

    Improve search query resolver

--- src/resolvers/search.js
+++ src/resolvers/search.js
@@ -12,22 +12,22 @@ exports.search = async (parent, args) => {
         user_id, community_id, age(now(), created_at) 
       FROM posts 
       WHERE to_tsvector(title || ' ' || COALESCE(description, '')) 
-        @@ to_tsquery($1)`,
+        @@ plainto_tsquery($1)`,
       [term]
     );
 
     const userQuery = await pool.query(
       `SELECT user_id, username, created_at, profile_pic_src 
       FROM users 
-      WHERE to_tsvector(username) @@ to_tsquery($1)`,
+      WHERE to_tsvector(username || ' u/' || username) @@ plainto_tsquery($1)`,
       [term]
     );
 
     const communityQuery = await pool.query(
       `SELECT community_id, name, title, description, created_at, logo_src 
       FROM communities 
-      WHERE to_tsvector(name || ' ' || title || ' ' || description) 
-        @@ to_tsquery($1)`,
+      WHERE to_tsvector(name || ' c/' || name || ' ' || title || ' ' || description) 
+        @@ plainto_tsquery($1)`,
       [term]
     );
 
